87db59e669aebc75448564c7e07a22489a5d1d9c
intervade <eotrusina@proton.me>
2024-02-18 23:40:32 -0600

    complete 01, 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(textInput, repeatAmount) {
+
+	var totalString = "";
+
+	if (repeatAmount >= 0) {
+		for (i = 1; i <= repeatAmount; i++) {
+			totalString = totalString + textInput;
+		}
+		return totalString;
+	}
+	else {
+		return "ERROR";
+	}
 
 };
 
