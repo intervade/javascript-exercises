bc08455d6148d970e9db888f51543f56713b942c
intervade <eotrusina@proton.me>
2024-02-21 00:39:12 -0600

    quick 06 for ya

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
-};
+const leapYears = function(inputYear) {
+	if (((inputYear % 100) == 0) && ((inputYear % 400) == 0)) {
+		return true;
+	}
+	else if (((inputYear % 4) == 0) && ((inputYear % 100) != 0)) {
+		return true;
+	}
+	else
+		return false;
+	}	
 
 // Do not edit below this line
 module.exports = leapYears;
